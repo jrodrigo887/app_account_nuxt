595e14f3898d7ee3999aded1da66d2c9c7701493
jrodrigo887 <jrodrigo887@gmail.com>
2023-07-07 21:41:52 -0300

    feat: instale e configure o tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <NuxtLayout name="default">
+      <h1 class="text-3xl font-bold underline bg-slate-900 text-white">
+        Hello world!
+      </h1>
       <NuxtPage />
     </NuxtLayout>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [join(currentDir, './assets/css/main.css')],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
